9b4814a72d62fc4e3fda83a45308faa31b07019c
Alex Hall <25327229+Ahall407@users.noreply.github.com>
2024-12-08 23:19:51 -0700

    final build for assig4p2

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,11 +6,11 @@
 ##############################################################
 
 #DONE: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'b7695bd23549283867257adccea4a0fc3380f0ee'
+AESD_ASSIGNMENTS_VERSION = c8a8d6f4fb39d7ab72c6d739c97aa6910ca52fd7
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignment-4-Ahall407.git'
+AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-Ahall407.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 

598ba3cf3ffcb2f953999051d157728e2482a6da
Alex Hall <25327229+Ahall407@users.noreply.github.com>
2024-12-10 11:06:34 -0700

    updated to point to updated assign3

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #DONE: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = b4970533f8ebc63f0ae5a3b4a3399cc2405659ac
+AESD_ASSIGNMENTS_VERSION = 7a69edcf9af0c321e96075fbcd77063d31ec8baa
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
